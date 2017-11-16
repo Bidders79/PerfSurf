93bb88425db8c1c784d27455b5a8b26c63379736
James Bidmead <jamesbidmead@me.com>
2017-11-16 11:04:59 +0000

    complete - not working
    complete - not working

---
+++ PerfSurf/PerfCounterWrapper/PerfCounterService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PerfSurf.PerfCounterWrapper
+{
+    public class PerfCounterService
+    {
+        public PerfCounterService()
+        {
+            _counters = new List<PerfCounterWrapper>();
+
+            _counters.Add(new PerfCounterWrapper("Processor", "Processor",
+                            "% Processor Time", "_Total"));
+
+            _counters.Add(new PerfCounterWrapper("Paging", "Memory",
+                            "Pages/sec"));
+
+            _counters.Add(new PerfCounterWrapper("Disk", "Phisical Disk",
+                            "% Disk Time", "_Total"));
+        }
+        public dynamic GetResults()
+        {
+            return _counters.Select(c => new
+                            { name = c.Name, value = c.Value });
+        }
+
+        List<PerfCounterWrapper> _counters;
+    }
+}
---
+++ PerfSurf/PerfCounterWrapper/PerfCounterWrapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace PerfSurf.PerfCounterWrapper
+{
+    public class PerfCounterWrapper
+    {
+        public PerfCounterWrapper(string name, string category, 
+                            string counter, string instance = "")
+        {
+            _counter = new PerformanceCounter(category, counter, 
+                                    instance, readOnly: true);
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public float Value
+        {
+            get
+            {
+                return _counter.NextValue();
+            }
+        }
+
+        private PerformanceCounter _counter;
+    }
+}
